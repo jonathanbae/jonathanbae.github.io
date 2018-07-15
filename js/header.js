4562cec4929121a4c0631a7d7a3e9e2a2fbfd0c6
jonathanbae <jonathanbae7@gmail.com>
2018-07-15 18:04:30 -0400

    navigation creation one

--- js/header.js
+++ js/header.js
@@ -1,10 +1,6 @@
-$(document).ready(function() {
-	//Load in the navigation bar
-	$("#header-placeholder").load("header.html");
-	//Set the active class
-	// $('nav.navbar').click(function(e) {
-	// 	// $('.nav li.active').removeClass('active');
-	// 	// $(this).parent().addClass('active');
-	// 	// e.preventDefault();
-	// });
-});
+
+
+
+function openResult(link){
+	location.href=link;
+}
