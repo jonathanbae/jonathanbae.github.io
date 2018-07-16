930478764209edcb7d6227bd06f81f8fa44451e3
jonathanbae <jonathanbae7@gmail.com>
2018-07-15 20:42:15 -0400

    navigation bottom shadow

--- README.md
+++ README.md
@@ -6,16 +6,23 @@ Here I will feature some stuff all about me.
 
 https://jonathanbae.github.io/index.html
 
-To launch website on local host: https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server
-Change to directory, on desktop:
-cd /d D:\Bae\Documents\jonathanbae.github.io
-cmd line: py -m http.server
-default to local host 8000
-
 To-Do List:
-[ ] JavaScript nav bar (highlight on pages selected)
-[ ] Make all sections mobile adaptable (made smaller)
-[ ] Run JS to load in text (from a text file or something)
-[ ] Host domain on own elsewhere
-[ ] Store weight usage on an a database/cloud db
-    [ ] run automatic graphs (potential gui for data)
+[] when past main - switch nav bar to dark and shadow
+[] load in images from google drive
+[] store link to download resume pdf
+[] add about me section
+[] add contact section
+[] add resume section
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
+[]
--- js/header.js
+++ js/header.js
@@ -1,6 +1,53 @@
 
 
-
+/**
+* Open the Link
+*/
 function openResult(link){
 	location.href=link;
 }
+
+/**
+* Closes Open/close the toggle button
+*/
+function closeToggler(){
+	$('#navbar-main-collapse').toggleClass('collapse');
+}
+
+/**
+* open the ID selected from navigation bar
+*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+						block: 'start',
+						inline: 'nearest'
+        });
+    });
+});
+
+/**
+* Check what div by ID is in view
+*/
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+/**
+* if main (name) is in view, do not show bottom shadow border
+*/
+$(window).on('resize scroll', function() {
+	if($('#main').isInViewport()){
+		$('#topNav').removeClass('bottom-shadow');
+	} else{
+		$('#topNav').addClass('bottom-shadow');
+	}
+});
