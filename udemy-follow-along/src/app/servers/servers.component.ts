ca3ab2c26d90016bfece0f79441a25b31692fc37
Unknown <jonathanbae7@gmail.com>
2019-12-14 20:07:43 -0700

    second section

--- udemy-follow-along/src/app/servers/servers.component.ts
+++ udemy-follow-along/src/app/servers/servers.component.ts
@@ -2,14 +2,32 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+
+  username = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() { }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  resetUsername() {
+    this.username = '';
+  }
 }
