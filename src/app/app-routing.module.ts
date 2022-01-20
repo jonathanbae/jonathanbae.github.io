38e9b3d1c2856cf781d0675d16cd6c2e4b5fab0a
Jonathan Bae <jonathanbae7@gmail.com>
2022-01-20 12:05:17 -0500

    shell components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LearnComponent } from './components/learn/learn.component';
+import { PracticeComponent } from './components/practice/practice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'learn', component: LearnComponent },
+  { path: 'practice', component: PracticeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LearnComponent } from './components/learn/learn.component';
+import { PracticeComponent } from './components/practice/practice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LearnComponent,
+    PracticeComponent
   ],
   imports: [
     BrowserModule,
