d98658db7bdad49390b83e1499c5e26594fb588c
Jonathan Bae <jonathanbae7@gmail.com>
2022-12-28 00:02:18 -0500

    redact

--- src/app/components/study/drug-learn-card/drug-learn-card.component.ts
+++ src/app/components/study/drug-learn-card/drug-learn-card.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Course, Drug } from 'src/assets/models/drug-models';
+import * as _ from "lodash";
+
+interface RedactedText {
+  class: string[];
+  administs: string[];
+  contras: string[];
+  warns: string;
+  boxed: string[];
+  precautions: string[];
+  adrs: string[];
+  points: string[];
+}
 
 @Component({
   selector: 'drug-learn-card',
@@ -9,6 +21,17 @@ import { Course, Drug } from 'src/assets/models/drug-models';
 export class DrugLearnCardComponent implements OnInit {
   @Input()
   drug: Drug = { id: 0, generic: 'This is a test' };
+
+  redactedTexts: RedactedText = {
+    class: [],
+    administs: [],
+    contras: [],
+    warns: '',
+    boxed: [],
+    precautions: [],
+    adrs: [],
+    points: [],
+  };
 
   newCourses: Set<Course> = new Set<Course>([
     Course['ICARE-NeuroPsych'],
@@ -26,5 +49,43 @@ export class DrugLearnCardComponent implements OnInit {
 
   get isNewContent() {
     return this.newCourses.has(this.drug.info!.course!);
+  }
+
+  redactAllFields() {
+    const info = _.cloneDeep(this.drug!.info!);
+    this.redactedTexts = {
+      class: info.class!,
+      administs: info.administs!,
+      contras: info.contras!,
+      warns: info.warns!,
+      boxed: info.boxed!,
+      precautions: info.precautions!,
+      adrs: info.adrs!,
+      points: info.points!,
+    };
+  }
+
+  isRedacted(text: string, field: string): boolean {
+    switch (field) {
+      case 'class':
+        return this.redactedTexts.class.includes(text);
+      default:
+        return false;
+    }
+  }
+
+  removeRedact(text: string, field: string) {
+    switch (field) {
+      case 'class':
+        this.updateRedactList(this.redactedTexts.class, text);
+        break;
+    }
+  }
+
+  updateRedactList(redacts: string[], text: string) {
+    const index = redacts.indexOf(text);
+    if (index > -1) {
+      redacts.splice(index, 1);
+    }
   }
 }
