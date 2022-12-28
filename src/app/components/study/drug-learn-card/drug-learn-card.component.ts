23f5579ae2ba0a8e808478611ea520b5792105b8
Jonathan Bae <jonathanbae7@gmail.com>
2022-12-27 20:29:55 -0500

    only show content per new/old

--- src/app/components/study/drug-learn-card/drug-learn-card.component.ts
+++ src/app/components/study/drug-learn-card/drug-learn-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Drug } from 'src/assets/models/drug-models';
+import { Course, Drug } from 'src/assets/models/drug-models';
 
 @Component({
   selector: 'drug-learn-card',
@@ -10,11 +10,21 @@ export class DrugLearnCardComponent implements OnInit {
   @Input()
   drug: Drug = { id: 0, generic: 'This is a test' };
 
+  newCourses: Set<Course> = new Set<Course>([
+    Course['ICARE-NeuroPsych'],
+    Course['ICARE-Gen Med II'],
+    Course['ICARE-SPECIAL POPS'],
+  ]);
+
   constructor() {}
 
   ngOnInit(): void {}
 
   prettify(val: string): string {
     return val.charAt(0).toUpperCase() + val.slice(1);
+  }
+
+  get isNewContent() {
+    return this.newCourses.has(this.drug.info!.course!);
   }
 }
